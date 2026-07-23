937d8f6c8c6d81b42cd2269cc0a718b61e7f68ac
Nisarg5400 <nisarggophane@gmail.com>
2026-07-23 22:25:50 +0530

    Add files via upload

---
+++ large_small_in3.py
@@ -0,0 +1,11 @@
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = max(a, b, c)
+smallest = min(a, b, c)
+
+print("Largest number:", largest)
+print("Smallest number:", smallest)
